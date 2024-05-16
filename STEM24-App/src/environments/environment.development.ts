c8c16a17b437dfcb18e10c7c0de2bbabd3b3dec1
tomislav <tomothevinkan3@gmail.com>
2024-05-16 09:22:50 +0200

    environments

--- STEM24-App/src/environments/environment.development.ts
+++ STEM24-App/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'https://6ce2-2a00-1bb8-118-1ac8-8156-31e2-1dca-6d7.ngrok-free.app/api/'
+    API_URL: 'https://stem-api.zoltan-palinkas.from.hr/api/'
 };
